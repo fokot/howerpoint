e32c285f26f37d183244a680fcafe662988ccd33
Frantisek Kocun <frantisek.kocun@merchenta.com>
2016-02-11 03:23:41 -0500

    Size settings

--- src/Howepoint.hs
+++ src/Howepoint.hs
@@ -6,7 +6,7 @@ import System.IO
 import Data.List.Split
 import Data.IORef
 import Control.DeepSeq (deepseq)
-import System.Console.Terminal.Size (size)
+import qualified System.Console.Terminal.Size as T
 import Data.Char (isLower, toLower)
 import Data.Maybe (isJust, fromMaybe)
 
@@ -14,12 +14,11 @@ f = "/Users/Frantisek/haskell/howerpoint/test/resources/howerpoint.txt"
 
 type Slide = [String]
 
-
-data PresentationSettings = PresentationSettings {
+data PresentationState = PresentationState {
+  slides :: [Slide],
   width :: Int,
-  height :: Int
---   slides :: [Slide],
---   currentSlide :: Int
+  height :: Int,
+  currentSlide :: Int
 }
 
 h = do
@@ -33,10 +32,10 @@ data Presentation = Presentation {
   p :: IO (),
   nn :: Int -> IO (),
   pp :: Int -> IO (),
-  g :: Int -> IO ()
---   resetSize :: IO (),
---   setSize :: Int -> Int -> IO (),
---   showSize :: IO ()
+  g :: Int -> IO (),
+  resetSize :: IO (),
+  setSize :: Int -> Int -> IO (),
+  showSize :: IO ()
 }
 
 parseSlides :: String -> IO [Slide]
@@ -46,36 +45,44 @@ parseSlides path = do
     slides `deepseq` hClose h
     pure slides
 
--- showSlide :: Slide -> IO ()
--- showSlide = mapM (putStrLn . formatSlide)
+displaySlide :: IORef PresentationState -> IO ()
+displaySlide ps = do
+  ps <- readIORef ps
+  let slide = (slides ps) !! (currentSlide ps)
+  mapM putStrLn (formatSlide ps slide) >> pure ()
 
-moveSlide :: [Slide] -> IORef Int -> (Int -> Int) -> IO ()
-moveSlide slides currentSlideRef f = do
-  modifyIORef currentSlideRef f
-  n <- readIORef currentSlideRef
-  let slide :: Slide = formatSlide (PresentationSettings 50 20) (slides !! n)
-  mapM putStrLn slide
-  pure ()
+moveSlide :: IORef PresentationState -> (Int -> Int) -> IO ()
+moveSlide ps f = do
+  modifyIORef ps (\p -> p {currentSlide = f (currentSlide p)})
+  displaySlide ps
 
+getTerminalSize :: IO (Int, Int)
+getTerminalSize = T.size >>= (\s -> pure $ maybe (50, 20) (\w -> (T.width w, T.height w)) s)
 
 loadPresentation :: String -> IO Presentation
 loadPresentation path = do
   slides <- parseSlides path
-  currentSlide <- newIORef 0
-  let slide :: Slide = formatSlide (PresentationSettings 50 20) (slides !! 0)
-  mapM putStrLn slide
+  (w, h) <- getTerminalSize
+  ioRef <- newIORef $ PresentationState slides w (h - 3) 0
+  displaySlide ioRef
   let maxSlides = length slides
-  let nextSlide = moveSlide slides currentSlide (\x -> if x == maxSlides - 1 then x else x + 1)
-  let previousSlide = moveSlide slides currentSlide (\x -> if x == 0 then x else x - 1)
-  let advanceSlides = \i -> moveSlide slides currentSlide (\x -> if x + i >= maxSlides - 1 then maxSlides - 1 else x + i)
-  let goBackSlides = \i -> moveSlide slides currentSlide (\x -> if x - i <= 0 then 0 else x - i)
-  let goToSlide = \i -> moveSlide slides currentSlide (\x -> if i > 0 && i <= maxSlides then i - 1 else x)
+  let nextSlide = moveSlide ioRef (\x -> if x == maxSlides - 1 then x else x + 1)
+  let previousSlide = moveSlide ioRef (\x -> if x == 0 then x else x - 1)
+  let advanceSlides = \i -> moveSlide ioRef (\x -> if x + i >= maxSlides - 1 then maxSlides - 1 else x + i)
+  let goBackSlides = \i -> moveSlide ioRef (\x -> if x - i <= 0 then 0 else x - i)
+  let goToSlide = \i -> moveSlide ioRef (\x -> if i > 0 && i <= maxSlides then i - 1 else x)
+  let resetSize = getTerminalSize >>= (\(w, h) -> modifyIORef ioRef (\p -> p {width = w, height = h - 3} ) ) >> displaySlide ioRef
+  let setSize w h = modifyIORef ioRef (\p -> p {width = w, height = h}) >> displaySlide ioRef
+  let showSize = readIORef ioRef >>= (\ps -> putStrLn $ (show $ width ps) ++ "x" ++ (show $ height ps) )
   return $ Presentation {
     n = nextSlide,
     p = previousSlide,
     nn = advanceSlides,
     pp = goBackSlides,
-    g = goToSlide
+    g = goToSlide,
+    resetSize = resetSize,
+    setSize = setSize,
+    showSize = showSize
   }
 
 -- |calculates left-right or top-bottom padding from the maximum size and content size
@@ -83,10 +90,9 @@ padding :: Int -> Int -> (Int, Int)
 padding size contentSize = (half, if odd (size - contentSize) then half + 1 else half)
   where half = floor $ (fromIntegral (size - contentSize)) / 2
 
-
 repeatN n = take n . repeat
 
-formatSlide :: PresentationSettings -> Slide -> Slide
+formatSlide :: PresentationState -> Slide -> Slide
 formatSlide ps content =
   line :  (repeatN topPadding emptyLine) ++ map centerLine content ++ repeatN bottomPadding emptyLine ++ [line]
   where (topPadding, bottomPadding) = padding (height ps) (length content)
@@ -126,7 +132,3 @@ colorize line = if elem '\\' line then colorize' line [] (length line) else (lin
                                             colorizedChar = fromMaybe [x] maybeColor
                                         in  colorize' xs (acc ++ colorizedChar) (if isJust maybeColor then len - 2 else len)
         colorize' (x:xs) acc len = colorize' xs (acc ++ [x]) len
-
-
--- gets the terminal size
--- import System.Console.Terminal.Size (size)
